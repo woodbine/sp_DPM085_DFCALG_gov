1dc0b6306ef37a0b6265aa83efac8c5b39937378
woodbine <ian@spendnetwork.com>
2015-04-09 22:38:08 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -43,7 +43,10 @@ for block in blocks:
 		fileUrl = fileUrl.replace(".csv/preview",".csv")
 		
 		title = fileBlock.h2.contents[0]
-		title = title.replace("(.csv format)","")
+		title = title.getText()
+		print title
+		
+		'''
 		
 		titleTest = title.find('Download CSV')
 		titleTest2 = title.find('DCLG expenditure')
@@ -63,7 +66,7 @@ for block in blocks:
 				csvMth = convert_mth_strings(csvMth);
 		
 				filename = entity_id + "_" + csvYr + "_" + csvMth
-		
+		'''
 				todays_date = str(datetime.now())
 		
 				scraperwiki.sqlite.save(unique_keys=['l'], data={"l": fileUrl, "f": filename, "d": todays_date })
